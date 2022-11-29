4e6107099de0dc73fe17ecdbf0274f11e6a56fec
lblake <treleven.lloyd@gmail.com>
2022-11-29 18:23:22 +0000

    fixed - getBusNumbers & - countSheep tests

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -14,11 +14,14 @@ export function isFromManchester(person) {
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error('people is required');
   // Your code here!
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error('arr is required');
   // Your code here!
+  let sheepCount = arr.filter((animal) => animal === 'sheep');
+  return sheepCount.length;
 }
 
 export function hasMPostCode(person) {
