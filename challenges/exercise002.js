f28ad4d5271d79b6ad349df7e1789eab3c10684b
lblake <treleven.lloyd@gmail.com>
2022-11-29 17:46:06 +0000

    fixed - getFillings & - isFromManchester tests

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,27 @@
 export function getFillings(sandwich) {
-  if (sandwich === undefined) throw new Error("ingredients is required");
+  if (sandwich === undefined) throw new Error('ingredients is required');
   // Your code here!
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
-  if (person === undefined) throw new Error("person is required");
+  if (person === undefined) throw new Error('person is required');
   // Your code here!
+  let location = person.city === 'Manchester' ? true : false;
+  return location;
 }
 
 export function getBusNumbers(people) {
-  if (people === undefined) throw new Error("people is required");
+  if (people === undefined) throw new Error('people is required');
   // Your code here!
 }
 
 export function countSheep(arr) {
-  if (arr === undefined) throw new Error("arr is required");
+  if (arr === undefined) throw new Error('arr is required');
   // Your code here!
 }
 
 export function hasMPostCode(person) {
-  if (person === undefined) throw new Error("person is required");
+  if (person === undefined) throw new Error('person is required');
   // Your code here!
 }
