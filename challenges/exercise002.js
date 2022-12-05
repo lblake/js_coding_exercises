98a9becdba2346188b5fc02b516921348b8cc69e
lblake <treleven.lloyd@gmail.com>
2022-12-05 23:25:38 +0000

    updated code

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,34 +1,33 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error('ingredients is required');
-  
+
   return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error('person is required');
-  
-  return person.city === 'Manchester' ? true : false;
-  
+
+  return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error('people is required');
-  
+
   return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error('arr is required');
-  
+
   let sheepCount = arr.filter((animal) => animal === 'sheep');
   return sheepCount.length;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error('person is required');
-  
-  return person.address.postCode.startsWith("M") &&
-    person.address.city === "Manchester"
+
+  return person.address.postCode.startsWith('M') &&
+    person.address.city === 'Manchester'
     ? true
     : false;
 }
