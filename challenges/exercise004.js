a1ad98ffcccb61d4066ed9cfb4e32860413507b7
lblake <treleven.lloyd@gmail.com>
2022-12-11 16:18:26 +0000

    refactored - arrShift test to be a more readable test

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,6 +1,6 @@
 export function findSmallNums(nums) {
   if (!nums) throw new Error('nums is required');
-  // Your code here
+
   var obj = {
     key: nums,
   };
@@ -15,34 +15,34 @@ export function findSmallNums(nums) {
 export function findNamesBeginningWith(names, char) {
   if (!names) throw new Error('names is required');
   if (!char) throw new Error('char is required');
-  // Your code here
+  
   const nameFirstCharacter = names.filter((names) => names.charAt(0) === char);
   return nameFirstCharacter;
 }
 
 export function findVerbs(words) {
   if (!words) throw new Error('words is required');
-  // Your code here
+  
   let verbs = words.filter((word) => word.startsWith('to '));
   return verbs;
 }
 
 export function getIntegers(nums) {
   if (!nums) throw new Error('nums is required');
-  // Your code here
+  
   let numsArray = nums.filter((num) => num % 1 == 0);
   return numsArray;
 }
 
 export function getCities(users) {
   if (!users) throw new Error('users is required');
-  // Your code here
+  
   return users.map((user) => user.data.city.displayName);
 }
 
 export function getSquareRoots(nums) {
   if (!nums) throw new Error('nums is required');
-  // Your code here
+  
   let squaredNumbers = nums.map((num) => Math.sqrt(num));
   let squaredRoundedNumbers = squaredNumbers.map(
     (num) => Math.round(num * 100) / 100
@@ -53,12 +53,12 @@ export function getSquareRoots(nums) {
 export function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error('sentences is required');
   if (!str) throw new Error('str is required');
-  // Your code here
+  
   return sentences.filter((sentence) => sentence.toLowerCase().includes(str));
 }
 
 export function getLongestSides(triangles) {
   if (!triangles) throw new Error('triangles is required');
-  // Your code here
+  
   return triangles.map((triangle) => Math.max(...triangle));
 }
--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -51,8 +51,9 @@ export const sumArrays = (arrs) => {
 export const arrShift = (arr) => {
   if (arr === undefined) throw new Error('arr is required');
 
-  [arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
-  return arr;
+  const inputCopy = [...arr];
+  const [first, last] = [inputCopy.splice(0, 1), inputCopy.splice(-1)];
+  return [...last, ...inputCopy, ...first];
 };
 
 export const findNeedle = (haystack, searchTerm) => {
